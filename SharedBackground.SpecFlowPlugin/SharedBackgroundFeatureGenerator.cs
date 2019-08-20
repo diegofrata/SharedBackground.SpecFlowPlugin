1a1968eed77e4ac87a62047147301e7af733fdc1
Diego Frata <diego.frata@starlizard.com>
2019-08-20 19:02:25 +0100

    Fix recursion issue

--- SharedBackground.SpecFlowPlugin/SharedBackgroundFeatureGenerator.cs
+++ SharedBackground.SpecFlowPlugin/SharedBackgroundFeatureGenerator.cs
@@ -20,27 +20,23 @@ namespace SharedBackground.SpecFlowPlugin
         static readonly Regex BackgroundRegex = new Regex("the background steps (?:of|in) '(?<Feature>.+)' have been executed");
         static readonly Regex ScenarioRegex = new Regex("the scenario '(?<Scenario>.+?)' (?:(?:of|in) '(?<Feature>.+?)' )?has been executed");
 
-        internal static bool CanGenerate(SpecFlowDocument document)
-        {
-            var canGenerate = document.SpecFlowFeature.Children
-                .Any(x =>
-                {
-                    var patterns = new[] { BackgroundRegex, ScenarioRegex };
+        internal static bool CanGenerate(SpecFlowDocument document) => document.SpecFlowFeature.Children.Any(CanGenerate);
 
-                    bool IsMatch(string text) => patterns.Any(a => a.IsMatch(text));
-                    
-                    switch (x)
-                    {
-                        case Background background:
-                            return IsMatch(background.Name ?? "");
-                        case StepsContainer stepsContainer:
-                            return stepsContainer.Steps.Any(step => IsMatch(step.Text ?? ""));
-                        default:
-                            return false;
-                    }
-                });
-            
-            return canGenerate;
+        static bool CanGenerate(IHasLocation x)
+        {
+            var patterns = new[] { BackgroundRegex, ScenarioRegex };
+
+            bool IsMatch(string text) => patterns.Any(a => a.IsMatch(text));
+
+            switch (x)
+            {
+                case Background background:
+                    return IsMatch(background.Name ?? "");
+                case StepsContainer stepsContainer:
+                    return stepsContainer.Steps.Any(step => IsMatch(step.Text ?? ""));
+                default:
+                    return false;
+            }
         }
 
         readonly SpecFlowConfiguration _generatorConfiguration;
@@ -64,82 +60,8 @@ namespace SharedBackground.SpecFlowPlugin
 
         public CodeNamespace GenerateUnitTestFixture(SpecFlowDocument document, string testClassName, string targetNamespace)
         {
-            StepsContainer GetBackgroundDocument(string text)
-            {
-                var patterns = new[] { BackgroundRegex, ScenarioRegex };
-
-                var match = patterns.Select(x => (Regex: x, Match: x.Match(text))).FirstOrDefault(x => x.Match.Success);
-
-                if (match == default)
-                    return null;
-                
-                var fileName = match.Match.Groups["Feature"].Value;
-
-                string filePath;
-                if (!string.IsNullOrEmpty(fileName))
-                {
-
-                    if (!fileName.EndsWith(".feature", StringComparison.InvariantCultureIgnoreCase))
-                        fileName += ".feature";
-
-                    filePath = Path.Combine(Path.GetDirectoryName(document.SourceFilePath), fileName);
-                }
-                else
-                    filePath = document.SourceFilePath;
-
-                var backgroundDocument = GenerateTestFileCode(new FeatureFileInput(filePath));
-
-                var hasScenario = ReferenceEquals(match.Regex, ScenarioRegex); 
-
-                return hasScenario 
-                    ? backgroundDocument?.SpecFlowFeature.ScenarioDefinitions.FirstOrDefault(x => x.Name == match.Match.Groups["Scenario"].Value) 
-                    : backgroundDocument?.SpecFlowFeature?.Background;
-            }
-            
-            var transformedChildren = document.SpecFlowFeature.Children.Select(x =>
-            {
-                switch (x)
-                {
-                    case Background background:
-                    {
-                        var backgroundDocument = GetBackgroundDocument(background.Name ?? "");
-                        return backgroundDocument ?? background;
-                    }
-                    case Scenario scenario:
-                    {
-                        var transformedSteps = scenario.Steps.SelectMany(step =>
-                        {
-                            var backgroundDocument = GetBackgroundDocument(step.Text ?? "");
-                            return backgroundDocument?.Steps ?? new[] { step };
-                        });
-
-                        if (scenario is ScenarioOutline scenarioOutline)
-                        {
-                            return new ScenarioOutline(
-                                scenarioOutline.Tags.ToArray(),
-                                scenarioOutline.Location,
-                                scenarioOutline.Keyword,
-                                scenarioOutline.Name,
-                                scenarioOutline.Description,
-                                transformedSteps.ToArray(),
-                                scenarioOutline.Examples.ToArray()
-                            );
-                        } 
-                        
-                        return new Scenario(
-                            scenario.Tags.ToArray(),
-                            scenario.Location,
-                            scenario.Keyword,
-                            scenario.Name,
-                            scenario.Description,
-                            transformedSteps.ToArray(),
-                            scenario.Examples.ToArray()
-                        );
-                    }
-                }
-                
-                return x;
-            });
+            var transformedChildren = document.SpecFlowFeature.Children
+                .Select(x => x is StepsContainer stepsContainer ? TransformSteps(document.SourceFilePath, stepsContainer) : x);
 
             var clonedDocument = new SpecFlowDocument(
                 new SpecFlowFeature(
@@ -155,9 +77,86 @@ namespace SharedBackground.SpecFlowPlugin
                 document.SourceFilePath
             );
 
-            return CanGenerate(clonedDocument) 
-                ? GenerateUnitTestFixture(clonedDocument, testClassName, targetNamespace) 
-                : _unitTestFeatureGenerator.GenerateUnitTestFixture(clonedDocument, testClassName, targetNamespace);
+            return _unitTestFeatureGenerator.GenerateUnitTestFixture(clonedDocument, testClassName, targetNamespace);
+        }
+
+        StepsContainer GetBackgroundDocument(string currentDocumentSourceFilePath, string text)
+        {
+            var patterns = new[] { BackgroundRegex, ScenarioRegex };
+
+            var match = patterns.Select(x => (Regex: x, Match: x.Match(text))).FirstOrDefault(x => x.Match.Success);
+
+            if (match == default)
+                return null;
+
+            var fileName = match.Match.Groups["Feature"].Value;
+
+            string filePath;
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                if (!fileName.EndsWith(".feature", StringComparison.InvariantCultureIgnoreCase))
+                    fileName += ".feature";
+
+                filePath = Path.Combine(Path.GetDirectoryName(currentDocumentSourceFilePath), fileName);
+            }
+            else
+                filePath = currentDocumentSourceFilePath;
+
+            var backgroundDocument = GenerateTestFileCode(new FeatureFileInput(filePath));
+
+            var hasScenario = ReferenceEquals(match.Regex, ScenarioRegex);
+
+            var stepsContainer = hasScenario
+                ? backgroundDocument?.SpecFlowFeature.ScenarioDefinitions.FirstOrDefault(x => x.Name == match.Match.Groups["Scenario"].Value)
+                : backgroundDocument?.SpecFlowFeature?.Background;
+
+            return CanGenerate(stepsContainer) ? TransformSteps(currentDocumentSourceFilePath, stepsContainer) : stepsContainer;
+        }
+
+        StepsContainer TransformSteps(string currentDocumentSourceFilePath, StepsContainer hasLocation)
+        {
+            switch (hasLocation)
+            {
+                case Background background:
+                {
+                    var backgroundDocument = GetBackgroundDocument(currentDocumentSourceFilePath, background.Name ?? "");
+                    return backgroundDocument ?? background;
+                }
+
+                case Scenario scenario:
+                {
+                    var transformedSteps = scenario.Steps.SelectMany(step =>
+                    {
+                        var backgroundDocument = GetBackgroundDocument(currentDocumentSourceFilePath, step.Text ?? "");
+                        return backgroundDocument?.Steps ?? new[] { step };
+                    });
+
+                    if (scenario is ScenarioOutline scenarioOutline)
+                    {
+                        return new ScenarioOutline(
+                            scenarioOutline.Tags.ToArray(),
+                            scenarioOutline.Location,
+                            scenarioOutline.Keyword,
+                            scenarioOutline.Name,
+                            scenarioOutline.Description,
+                            transformedSteps.ToArray(),
+                            scenarioOutline.Examples.ToArray()
+                        );
+                    }
+
+                    return new Scenario(
+                        scenario.Tags.ToArray(),
+                        scenario.Location,
+                        scenario.Keyword,
+                        scenario.Name,
+                        scenario.Description,
+                        transformedSteps.ToArray(),
+                        scenario.Examples.ToArray()
+                    );
+                }
+            }
+
+            return hasLocation;
         }
 
         SpecFlowDocument GenerateTestFileCode(FeatureFileInput featureFileInput)
